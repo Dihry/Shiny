ec1cdaf046bebbe4feb0fe94d286f6865fb68674
Dihry <edgeqode@gmail.com>
2022-06-19 15:56:21 +0300

    popup

---
+++ JS/popup.js
@@ -0,0 +1,77 @@
+const popupLink = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+
+let unlock = true;
+
+const timeout = 800;
+
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+if(popupCloseIcon.length > 0){
+   for(let index = 0; index < popupCloseIcon.length; index++){
+      const el = popupCloseIcon[index];
+      el.addEventListener('click', function (e) {
+         popupCloseIcon(el.closest('.popup'));
+         e.preventDefault();
+      });
+   }
+}
+
+function popupOpen(currentPopup){
+   if(currentPopup && unlock){
+      const popupActive = document.querySelector('.popup.open');
+      if(popupActive){
+         popupClose(popupActive, false);
+      }else{
+         bodylock();
+      }
+      currentPopup.classList.add('open');
+      currentPopup.addEventListener("click", function(e){
+         if(!e.target.closest('.popup__content')){
+            popupClose(e.target.closest('.popup'));
+         }
+      });
+   }
+}
+
+function popupClose(popupActive, doUnlock = true){
+   if(unlock){
+      popupActive.classList.remove('open');
+      if(doUnlock){
+         bodyUnlock();
+      }
+   }
+}
+
+function bodylock(){
+   const lockPaddingValue = window.innerWidth - document.querySelector('body').offsetWidth + 'px';
+
+   for (let index = 0; index < lockPadding.length; index ++){
+      const el = lockPadding[index];
+      el.style.lockPaddingRight = lockPaddingValue;
+   }
+   body.style.paddingRight - lockPaddingValue;
+   body.classList.add('lock');
+
+   unlock = false;
+   setTimeout(function(){
+      unlock = true;
+   }, timeout);
+}
+
+function bodyUnlock(){
+   setTimeout(function() {
+      if(lockPadding.length > 0) {
+         for (let index = 0; index < lockPadding.length; index++){
+            const el = lockPadding[index];
+            el.style.paddingRight = '0px';
+         }
+      }
+      body.style.paddingRight = '0px';
+      body.classList.remove('lock');
+   }, timeout);
+   unlock = false;
+   setTimeout(function () {
+      unlock = true;
+   }, timeout);
+};
